c3d0ea17a162791d03b9fb5944fda4c32c4c319a
andriuszzz <andre.genolima@gmail.com>
2023-06-05 21:19:55 -0300

    bug fixing

--- Views/Modelo/ModeloView.cs
+++ Views/Modelo/ModeloView.cs
@@ -1,72 +1,36 @@
-// using Models;
-// using Controllers;
+using Models;
+using Controllers;
 
-<<<<<<< HEAD
-// namespace Views{
-//     public class ListModelo : From{
-=======
+        
+        
 namespace Views{
-    public class ListModelo : Form {
->>>>>>> c3b9309cf20c929fcf62d71aa5196aaa3e7de543
+    public class ListModelo : From{
 
-//         public enum option {Upgrade, Delete}
+        public enum option {Upgrade, Delete}
 
-<<<<<<< HEAD
-//         ListView ListModelo;
+        ListView ListModelo;
 
-//         private void AddListView(Models.ModeloModels produto){
-=======
-        ListView ListModeloView;
+        private void AddListView(Models.ModeloModels produto){
 
-        private void AddListView(Models.ModeloModels modelo){
->>>>>>> c3b9309cf20c929fcf62d71aa5196aaa3e7de543
+        string[]row = {
 
-//             string[]row = {
-
-<<<<<<< HEAD
-//                 modelo.id,
-//                 modelo.modelo
-//             };
-
-//             ListViewItem item = new ListViewItem(row);
-//             listProduto.Items.Add(item);
-//         }
-
-//         public void RefreshList()
-//         {
-//             listModelo.Items.Clear();
-=======
-                modelo.Id.ToString(),
-                modelo.Modelo
+            modelo.id.ToString(),
+            modelo.Modelo 
             };
 
             ListViewItem item = new ListViewItem(row);
-            // TODO: Criar lista
-            // listProduto.Items.Add(item);
+            // TODO> Criar lista
         }
-
         public void RefreshList()
         {
-            // TODO: Criar lista
-            // listModelo.Items.Clear();
->>>>>>> c3b9309cf20c929fcf62d71aa5196aaa3e7de543
+            // TODO> Criar lista
 
-//             List<Models.ModeloModels> list = Controllers.ModeloController.Read();
+            List<Models.ModeloModels> list = Controllers.ModeloController.Read();
 
-<<<<<<< HEAD
-//             foreach (Models.ProdutoModels produto in list)
-//             {
-//                 AddListView(modelo);
-//             }
-//         }
-=======
-            foreach (Models.ModeloModels modelo in list)
+            foreach (Models.ProdutoModels produto in list)
             {
                 AddListView(modelo);
             }
         }
->>>>>>> c3b9309cf20c929fcf62d71aa5196aaa3e7de543
-
-    
-//     }
-// }
+    }
+}
--- Views/Produto/ProdutoView.cs
+++ Views/Produto/ProdutoView.cs
@@ -34,7 +34,5 @@ namespace Views{
                 AddListView(produto);
             }
         }
-
-    
     }
 }
