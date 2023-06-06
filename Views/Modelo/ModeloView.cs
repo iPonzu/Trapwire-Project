b5a55b2597efcad5dc51d74901501e7c4a3e8055
andriuszzz <andre.genolima@gmail.com>
2023-06-05 21:43:22 -0300

    merging

--- Views/Modelo/ModeloView.cs
+++ Views/Modelo/ModeloView.cs
@@ -1,58 +1,33 @@
 using Models;
 using Controllers;
-
-<<<<<<< HEAD
         
         
 namespace Views{
     public class ListModelo : From{
-=======
-// namespace Views{
-//     public class ListModelo : From{
->>>>>>> 395ddfe4c1e11b0058fabd747f7fb5d2b86c94c7
 
         public enum option {Upgrade, Delete}
 
-<<<<<<< HEAD
+
         ListView ListModelo;
 
         private void AddListView(Models.ModeloModels produto){
 
-        string[]row = {
+            string[]row = {
 
             modelo.id.ToString(),
             modelo.Modelo 
             };
 
             ListViewItem item = new ListViewItem(row);
-            // TODO> Criar lista
+                // TODO> Criar lista
         }
         public void RefreshList()
         {
-            // TODO> Criar lista
-=======
-//         ListView ListModelo;
-
-//         private void AddListView(Models.ModeloModels produto){
-
-//             string[]row = {
-
-//                 modelo.id,
-//                 modelo.modelo
-//             };
-
-//             ListViewItem item = new ListViewItem(row);
-//             listProduto.Items.Add(item);
-//         }
-
-//         public void RefreshList()
-//         {
-//             listModelo.Items.Clear();
->>>>>>> 395ddfe4c1e11b0058fabd747f7fb5d2b86c94c7
+                // TODO> Criar lista
 
             List<Models.ModeloModels> list = Controllers.ModeloController.Read();
 
-<<<<<<< HEAD
+
             foreach (Models.ProdutoModels produto in list)
             {
                 AddListView(modelo);
@@ -60,14 +35,3 @@ namespace Views{
         }
     }
 }
-=======
-//             foreach (Models.ProdutoModels produto in list)
-//             {
-//                 AddListView(modelo);
-//             }
-//         }
-
-    
-//     }
-// }
->>>>>>> 395ddfe4c1e11b0058fabd747f7fb5d2b86c94c7
