cd3110f176cd7f548bbf9c6f1d83620980820e81
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-07-10 20:32:54 -0300

    configuring index

--- index.cs
+++ index.cs
@@ -4,7 +4,6 @@ namespace Program{
     public class Program{
         public static void Main(string[] args){
             Application.EnableVisualStyles();
-            // Application.Run(new CategoriaView());
             Menu.index();
         }
     }
