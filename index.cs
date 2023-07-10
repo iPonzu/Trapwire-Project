bb39174b98539c0b99621528f48198c134c09f75
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-07-10 20:32:33 -0300

    visual config

--- index.cs
+++ index.cs
@@ -4,8 +4,8 @@ namespace Program{
     public class Program{
         public static void Main(string[] args){
             Application.EnableVisualStyles();
-            Application.Run(new CategoriaView());
-            // Menu.index();
+            // Application.Run(new CategoriaView());
+            Menu.index();
         }
     }
 }
